76f86d7bf0303a98eb72ba6372b412fb83db7891
ztao <649949135@qq.com>
2024-07-17 10:29:25 +0800

    feat: ag-grid大数据量

--- src/components/Aggrid/index.vue
+++ src/components/Aggrid/index.vue
@@ -1,4 +1,5 @@
 <script setup>
+  import { ref, onMounted } from 'vue';
   import { AgGridVue } from 'ag-grid-vue3'; // Vue Grid Logic
 
   import 'ag-grid-enterprise';
@@ -8,17 +9,33 @@
   LicenseManager.setLicenseKey(
     'IRDEVELOPERS_COM_NDEwMjM0NTgwMDAwMA==f08aae16269f416fe8d65bbf9396be5f'
   );
+  //定义数据
+  const rowData = ref([]);
+  onMounted(async () => {
+    let fetchData = await fetch(
+      'https://www.ag-grid.com/example-assets/olympic-winners.json'
+    );
+    let newData = await fetchData.json();
+    rowData.value = newData;
+  });
+
+  //自定义列属性
 
   const columnDefs = [
-    { field: 'make' },
-    { field: 'model' },
-    { field: 'price' }
-  ];
-
-  const rowData = [
-    { make: 'Toyota', model: 'Celica', price: 35000 },
-    { make: 'Ford', model: 'Mondeo', price: 32000 },
-    { make: 'Porsche', model: 'Boxster', price: 72000 }
+    {
+      field: 'athlete',
+      filter: 'agMultiColumnFilter',
+      headerName: '运动员',
+      enableRowGroup: true
+    },
+    { field: 'age', filter: true, headerName: '年龄', enableRowGroup: true },
+    { field: 'date', filter: true, headerName: '生日' },
+    { field: 'country', filter: 'agSetColumnFilter', headerName: '国家' },
+    { field: 'sport', filter: 'agMultiColumnFilter', headerName: '运动项目' },
+    { field: 'gold', filter: 'agNumberColumnFilter', headerName: '金牌' },
+    { field: 'silver', filter: 'agNumberColumnFilter', headerName: '银牌' },
+    { field: 'bronze', filter: 'agNumberColumnFilter', headerName: '铜牌' },
+    { field: 'total', filter: false, headerName: '总分', aggFunc: 'sum' }
   ];
 
   const defaultColDef = {
@@ -36,14 +53,17 @@
 </script>
 
 <template>
-  <div style="width: 700px">
+  <div style="width: 1800px">
     <ag-grid-vue
       :localeText="$localeText"
       :rowData="rowData"
       :columnDefs="columnDefs"
-      style="height: 500px"
+      :sideBar="true"
+      :enableRangeSelection="true"
+      :enableCharts="true"
       :defaultColDef="defaultColDef"
       :statusBar="statusBar"
+      style="height: 500px"
       class="ag-theme-quartz"
     >
     </ag-grid-vue>
