783e2301a22586e5a9e4457734f17b688014930b
ztao <taotao.zhang@epean.com.cn>
2025-05-16 18:05:46 +0800

    feat: 部分修改

--- src/views/SchemaPage.vue
+++ src/views/SchemaPage.vue
@@ -2,26 +2,20 @@
   <div>
     <h1>Schema Page</h1>
   </div>
-  <VueForm v-if="schema" v-model="formData" :schema="schema"> </VueForm>
-  <div v-else class="loading">
-    <el-skeleton :rows="5" animated />
-    <p>正在加载表单配置...</p>
-  </div>
+
 </template>
 
 <script setup>
-  import VueForm from '@lljj/vue3-form-element';
   import { ElMessage } from 'element-plus';
-  import { ref, onMounted } from 'vue';
+  import { onMounted } from 'vue';
 
-  const formData = ref({});
-  const schema = ref(null);
+
 
   const loadSchema = async () => {
     try {
       const response = await fetch('src/assets/demo.json');
       const data = await response.json();
-      schema.value = data;
+      console.log('schema.json 内容:', data);
     } catch (error) {
       console.error('加载 schema.json 失败:', error);
       ElMessage.error('加载 schema.json 失败，请检查文件格式');
